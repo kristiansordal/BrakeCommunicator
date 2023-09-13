8504217b9ad89b09bc13e63fdc1a25a1bc1ddc81
Kristian Sørdal <krisor99@gmail.com>
2023-09-13 09:19:44 +0200

    works but vector gets shuffled

--- mat-vec-mult/main.c
+++ mat-vec-mult/main.c
@@ -4,50 +4,46 @@
 
 // Initialize a matrix segment
 void init_matrix_segment(double *matrix, int matSize, int n, int rank) {
+    // Value of j is set in order to achieve index(i,j) = i + j
     for (int i = 0, j = rank * matSize / n; i < matSize; i++) {
         if (i > 0 && i % n == 0) {
             j++;
         }
         matrix[i] = (i % n) + j + 1;
     }
-    printf("\n");
 }
 
-double *mat_mul(double *matrix, double *vector, /* double *res, */ int matSize, int n) {
+void mat_mul(double *matrix, double *vector, double *res, int matSize, int n, int rank) {
     int rows = matSize / n;
-    double *res = (double *)malloc(rows * sizeof(double));
+    int startRow = rank * rows;
 
-    // TODO: Handle error case when matSize == n (or something idk)
-    for (int i = 0; i < matSize; i++) {
-        matrix[i] *= vector[i % n];
-    }
+    printf("Res Mem adr: %p\n", (void *)&res[0]);
+    for (int i = 0; i < rows; i++) {
+        double sum = 0;
 
-    double sum = 0;
-
-    for (int i = 1, j = 0; i <= matSize; i++) {
-        sum += matrix[i - 1];
-        if (i % n == 0) {
-            res[j] = sum;
-            sum = 0;
-            j++;
+        for (int j = 0; j < n; j++) {
+            printf("%f, %f\n", matrix[i * n + j], vector[j]);
+            sum += matrix[i * n + j] * vector[j];
         }
-    }
 
-    return res;
+        printf("Inserting %f into res at id: %d\n", sum, startRow + i);
+        res[startRow + i] = sum;
+    }
 }
+
 int main(int argc, char **argv) {
     int rank;
     int np;
-    MPI_Comm world = MPI_COMM_WORLD;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(world, &rank);
-    MPI_Comm_size(world, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-    const int scale = 10;
+    const int scale = 2;
     const int n = 1 << scale;
 
     double *vector = (double *)malloc(n * sizeof(double));
+    double *res = (double *)malloc(n * sizeof(double));
 
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
@@ -56,22 +52,24 @@ int main(int argc, char **argv) {
     }
     double start = MPI_Wtime();
 
-    MPI_Bcast(vector, n, MPI_DOUBLE, 0, world);
+    MPI_Bcast(vector, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     int matSize = (n * n) / np;
 
     // Creates a matrix segment
     double *matrix = (double *)malloc(matSize * sizeof(double));
-    init_matrix_segment(matrix, matSize, n, rank);
 
-    double *res = mat_mul(matrix, vector, matSize, n);
+    init_matrix_segment(matrix, matSize, n, rank);
+    mat_mul(matrix, vector, res, matSize, n, rank);
 
     double end = MPI_Wtime();
 
-    printf("Scale: %d\nTime taken: %f", scale, end - start);
+    printf("Scale: %d\nTime taken: %f\n", scale, end - start);
 
-    for (int i = 0; i < 10 / n; i++) {
-        printf("%2.2f ", res[i]);
+    for (int i = 0; i < n; i++) {
+        if (res[i] > 0.0000000000) {
+            printf("%2.2f ", res[i]);
+        }
     }
 
     MPI_Finalize();

6e6b104fb87ef143a7c86debea78384a1913cac7
Kristian Sørdal <krisor99@gmail.com>
2023-09-13 10:59:54 +0200

    works goodie goodie now

--- mat-vec-mult/main.c
+++ mat-vec-mult/main.c
@@ -13,33 +13,31 @@ void init_matrix_segment(double *matrix, int matSize, int n, int rank) {
     }
 }
 
-void mat_mul(double *matrix, double *vector, double *res, int matSize, int n, int rank) {
+void mat_mul(double *matrix, double *vector, double *res, int matSize, int n) {
     int rows = matSize / n;
-    int startRow = rank * rows;
 
-    printf("Res Mem adr: %p\n", (void *)&res[0]);
     for (int i = 0; i < rows; i++) {
         double sum = 0;
 
         for (int j = 0; j < n; j++) {
-            printf("%f, %f\n", matrix[i * n + j], vector[j]);
             sum += matrix[i * n + j] * vector[j];
         }
 
-        printf("Inserting %f into res at id: %d\n", sum, startRow + i);
-        res[startRow + i] = sum;
+        res[i] = sum;
     }
 }
 
 int main(int argc, char **argv) {
     int rank;
     int np;
+    double start;
+    double end;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-    const int scale = 2;
+    const int scale = 15;
     const int n = 1 << scale;
 
     double *vector = (double *)malloc(n * sizeof(double));
@@ -49,8 +47,8 @@ int main(int argc, char **argv) {
         for (int i = 0; i < n; i++) {
             vector[i] = i + 1;
         }
+        start = MPI_Wtime();
     }
-    double start = MPI_Wtime();
 
     MPI_Bcast(vector, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
@@ -60,14 +58,23 @@ int main(int argc, char **argv) {
     double *matrix = (double *)malloc(matSize * sizeof(double));
 
     init_matrix_segment(matrix, matSize, n, rank);
-    mat_mul(matrix, vector, res, matSize, n, rank);
 
-    double end = MPI_Wtime();
+    mat_mul(matrix, vector, res, matSize, n);
 
-    printf("Scale: %d\nTime taken: %f\n", scale, end - start);
+    free(matrix);
+    free(vector);
 
-    for (int i = 0; i < n; i++) {
-        if (res[i] > 0.0000000000) {
+    MPI_Gather(res, matSize / n, MPI_DOUBLE, res, matSize / n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // double prev = 0;
+    if (rank == 0) {
+        end = MPI_Wtime();
+        printf("Scale: %d\nTime taken: %f\n", scale, end - start);
+        for (int i = 0; i < 10; i++) {
+            // if (prev > res[i]) {
+            //     printf("ERROR: PREV LARGER THAN RES");
+            // }
+            // prev = res[i];
             printf("%2.2f ", res[i]);
         }
     }

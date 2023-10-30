cf27443657a6dd205fbcb3064890358360db727e
Kristian Sørdal <krisor99@gmail.com>
2023-10-30 08:20:35 +0100

    stuff

--- sparse-matrix/par_d/main.cpp
+++ sparse-matrix/par_d/main.cpp
@@ -2,11 +2,10 @@
 #include "ellpack.h"
 
 int main() {
-    int n = 1 << 10;
+    int n = 1 << 12;
     int timesteps = 100;
     double start, end;
 
-    std::cout << n << std::endl;
     ELLpack<double> ellpack(n);
     ellpack.initialize();
     ellpack.initialize_stiffness_matrix();
@@ -16,19 +15,30 @@ int main() {
     if (ellpack.rank == 0) {
         start = ellpack.time.elapsed();
     }
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < timesteps; i++) {
         ellpack.update();
     }
 
+    std::vector<double> vv;
+
+    mpi::gather(ellpack.world, ellpack.v_new.data(), ellpack.size_rank(), vv, 0);
     if (ellpack.rank == 0) {
+        double ops = (long long)n * 8ll * 100ll;
+        double l2 = 0;
+
+        for (auto &v : vv) {
+            l2 += v * v;
+        }
+
+        l2 = std::sqrt(l2);
+
         end = ellpack.time.elapsed();
         std::cout << "Time:    " << end - start << std::endl;
         std::cout << "Comp:    " << (end - start) - ellpack.comm_time << std::endl;
         std::cout << "Comm:    " << ellpack.comm_time << std::endl;
-        std::cout << "GFLOPS:  " << ellpack.comm_time << std::endl;
+        std::cout << "GFLOPS:  " << ops / (end - start) << std::endl;
+        std::cout << "L2:      " << l2 << std::endl;
     }
 
-    double ops = (long long)n * 8ll * 100ll;
-    std::cout << ops << std::endl;
     return 0;
 }

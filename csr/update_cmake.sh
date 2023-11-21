a14b774681ec7c9123a32b4bef56c9b18b271fd8
Kristian Sørdal <krisor99@gmail.com>
2023-11-21 08:46:57 +0100

    updated .gitignore and submodules

--- csr/update_cmake.sh
+++
@@ -1,5 +0,0 @@
-!#/bin/bash
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_OSX_ARCHITECTURES=arm64 -B build/debug
-rm compile_commands.json
-ln -s build/debug/compile_commands.json compile_commands.json
-cmake --build build/debug

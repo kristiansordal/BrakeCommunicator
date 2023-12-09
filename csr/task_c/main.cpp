0a45279a34d8d7fab5d8c210583ea2c48862512a
Kristian Sørdal <krisor99@gmail.com>
2023-12-09 21:11:01 +0100

    test

--- csr/task_c/main.cpp
+++ csr/task_c/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
     // MPI_Init(&argc, &argv);
     int rank, np;
 
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 

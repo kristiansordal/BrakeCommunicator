368060d22f49dc2518b82279d35e2fed8b57f325
Kristian Sørdal <krisor99@gmail.com>
2023-12-09 15:48:28 +0100

    update build.sh

--- csr/build.sh
+++ csr/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_OSX_ARCHITECTURES=arm64 -B build/debug && cmake --build build/debug && ln -s build/debug/compile_commands.json compile_commands.json
+rm -rf build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_OSX_ARCHITECTURES=arm64 -B build/debug && cmake --build build/debug && rm compile_commands.json && ln -s build/debug/compile_commands.json compile_commands.json

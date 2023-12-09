40ffcadad4e5ae9452fea7000feb546ec5d60f86
Kristian Sørdal <krisor99@gmail.com>
2023-12-09 19:02:46 +0100

    broadcast

--- csr/task_c/main.cpp
+++ csr/task_c/main.cpp
@@ -19,10 +19,15 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
     vector<i64> row_ptr;
+    vector<i64> row_ids;
     vector<i64> col_ptr;
     vector<double> vals;
+    vector<i64> rc;
     int nrows, ncols;
 
+    rc.assign(np, 0);
+
+    // Read file on all ranks
     ifstream file;
     file.open(argv[1]);
 
@@ -35,7 +40,7 @@ int main(int argc, char **argv) {
 
     cout << "File successfully opened" << endl;
 
-    ffm::read_matrix_market_triplet(file, nrows, ncols, col_ptr, row_ptr, vals);
+    ffm::read_matrix_market_triplet(file, nrows, ncols, col_ptr, row_ids, vals);
 
     cout << "Successfully read file" << endl;
 
@@ -43,32 +48,52 @@ int main(int argc, char **argv) {
     int n = nrows;
 
     file.close();
+    // Done reading files
+
+    // Initialize row pointer
+    vector<i64> r;
+    int count = 1;
+
+    r.push_back(0);
+    for (int i = 1; i < row_ids.size(); i++) {
+        if (row_ids[i] == row_ids[i - 1]) {
+            count++;
+        } else {
+            r.push_back(count++);
+        }
+    }
+
+    r.push_back(count);
+    row_ptr = r;
+    // Done initializing row ptr
 
     MPI_Barrier(MPI_COMM_WORLD);
+
+    // Start load balancing
     if (rank == 0) {
-        cout << np << endl;
-        for (int i = 1; i < np; i++) {
-            MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        int avg_load = nnz / np;
+        int core = 0;
+
+        for (int i = 0; i < nrows; i++) {
+            rc[core]++;
+
+            if (row_ptr[i + 1] > avg_load * (core + 1)) {
+                core++;
+            }
         }
-
-    } else {
-        n = 0;
-        MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        cout << rank << " has " << n << endl;
+        for (auto &i : rc) {
+            cout << i << " ";
+        }
+        cout << endl;
     }
-    // vector<i64> r;
-    // int count = 1;
 
-    // r.push_back(0);
-    // for (int i = 1; i < row_ptr.size(); i++) {
-    //     if (row_ptr[i] == row_ptr[i - 1]) {
-    //         count++;
-    //     } else {
-    //         r.push_back(count++);
-    //     }
-    // }
-    // r.push_back(count);
-    // row_ptr = r;
+    MPI_Bcast(rc.data(), np * sizeof(int), MPI_INT, 0, MPI_COMM_WORLD);
+
+    for (auto &i : rc) {
+        cout << i << " ";
+    }
+    cout << endl;
+    // End load balancing
 
     return 0;
 }

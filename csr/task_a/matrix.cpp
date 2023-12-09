e184084da3126d7b97909462bd629f2f4c8ca3da
Kristian Sørdal <krisor99@gmail.com>
2023-12-09 16:42:21 +0100

    went back to old send strat

--- csr/task_a/matrix.cpp
+++ csr/task_a/matrix.cpp
@@ -15,21 +15,43 @@ void Matrix::update(mpi::communicator &world, mpi::timer &time, int rank, double
     }
     tcomp_kernel += time.elapsed() - t1;
 
-    vector<int> recvcounts(world.size(), 0);
-    int v = v_new.size();
-    mpi::all_gather(world, &v, 1, recvcounts);
-    vector<int> displs;
-    displs.push_back(0);
-    for (int i = 1; i < recvcounts.size(); i++) {
-        displs[i] = displs[i - 1] + recvcounts[i - 1];
+    // vector<int> recvcounts(world.size(), 0);
+    // int v = v_new.size();
+    // mpi::all_gather(world, &v, 1, recvcounts);
+    // vector<int> displs;
+    // displs.push_back(0);
+    // for (int i = 1; i < recvcounts.size(); i++) {
+    //     displs[i] = displs[i - 1] + recvcounts[i - 1];
+    // }
+
+    // // Resize v_old to accommodate the gathered data
+    // v_old.clear();
+    // v_old.assign(nrows, 0);
+
+    // // MPI_Allgatherv for integer data
+    // MPI_Allgatherv(v_new.data(), v, MPI_DOUBLE, v_old.data(), recvcounts.data(), displs.data(), MPI_DOUBLE, world);
+    //
+    vector<vector<double>> send_buff;
+    send_buff.assign(world.size(), vector<double>());
+    send_buff[rank] = v_new;
+    t1 = time.elapsed();
+    for (int i = 0; i < world.size(); i++) {
+        if (i == rank) {
+            for (int j = 0; j < world.size(); j++) {
+                if (j != rank) {
+                    world.send(j, i, send_buff[i]);
+                }
+            }
+        } else {
+            world.recv(i, i, send_buff[i]);
+        }
     }
-
-    // Resize v_old to accommodate the gathered data
+    tcomm += time.elapsed() - t1;
     v_old.clear();
-    v_old.assign(nrows, 0);
-
-    // MPI_Allgatherv for integer data
-    MPI_Allgatherv(v_new.data(), v, MPI_DOUBLE, v_old.data(), recvcounts.data(), displs.data(), MPI_DOUBLE, world);
+    v_old.reserve(nrows);
+    for (auto &i : send_buff) {
+        v_old.insert(v_old.end(), i.begin(), i.end());
+    }
     tcomm += time.elapsed() - t1;
 }
 
